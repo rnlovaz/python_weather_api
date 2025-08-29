2c1d691e6406a6de26daa165956f06acb9c457b9
Ricardo Vaz <rvaz@imaginarycloud.com>
2025-08-29 18:33:51 +0100

    feat: adds logic to some repo functions

--- src/api/location/repository.py
+++ src/api/location/repository.py
@@ -1,19 +1,29 @@
-from sqlmodel import Session
-# from .entities import Location
+from sqlmodel import Session, select
+
+from .entities import Location
 
 
 class LocationRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def get_all(self):  # -> list[Location]:
-        pass
+    def get_all(self) -> list[Location]:
+        return list(self.session.exec(select(Location)).all())
 
-    def get_one(self, slug: str):  # -> Location:
-        pass
+    def get_one(self, slug: str) -> Location | None:
+        return self.session.exec(
+            select(Location).where(Location.slug == slug)
+        ).one_or_none()
 
-    def create(self, slug: str, lat: float, lon: float):  # -> Location:
-        pass
+    def create(self, slug: str, lat: float, lon: float) -> Location:
+        new_location = Location(slug=slug, latitude=lat, longitude=lon)
+        self.session.add(new_location)
+        self.session.commit()
+        self.session.refresh(new_location)
+        return new_location
+
+    # def update(self, slug: str, lat: Optional[float], lon: Optional[float]) -> Location:
+    #     pass
 
     def delete(self, slug: str) -> None:
         pass
